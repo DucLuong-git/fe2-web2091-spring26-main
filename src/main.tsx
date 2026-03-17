380eef3c0694647366c41e31498288dc5b7c3743
DucLuong <lt1739274@gmail.com>
2026-03-17 16:00:47 +0700

    lab4

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import "./index.css";
 import App from "./App";
 import Lab1 from "./pages/Lab1";
 import Lab2 from "./pages/Lab2";
 import Lab3 from "./pages/Lab3";
+import StoryForm from "./pages/Lab4";
 import "antd/dist/reset.css";
 
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <Lab3 />
+     <QueryClientProvider client={queryClient}>
+        <StoryForm />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 );
---
+++ src/pages/Lab4.tsx
@@ -0,0 +1,130 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { Button, Form, Input, Checkbox, Layout, Select } from "antd";
+import { Link } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import axios from "axios";
+import toast from "react-hot-toast";
+interface Category {
+    id: number;
+    title: string;
+}
+interface Stories{
+    title:string;
+    description:string;
+    author:string;
+    Active?:Boolean;
+    categoryId: number;
+}
+export default function StoryForm(){
+    const {mutate, isPending}=useMutation({
+        mutationFn: async(value:Stories)=>{
+            await axios.post("http://localhost:3000/stories",value)
+        },
+        onSuccess:()=>{
+            toast.success("story true")
+        },
+        onError:()=>{
+            toast.error("story false")
+        }
+    })
+    const { data: categories, isLoading } = useQuery({
+    queryKey: ["categories"],
+    queryFn: async () => {
+        const res = await axios.get("http://localhost:3000/categories");
+        return res.data;
+    }
+    })
+    const onFinish =(value:Stories)=>{
+        mutate(value)
+    }
+    const { Header, Content, Footer } = Layout;
+    return (
+        <>
+      <nav className="bg-blue-600 text-white shadow">
+        <div className="max-w-6xl mx-auto px-4 h-16 flex items-center justify-between">
+          <Link to="#" className="text-xl font-semibold">
+            <strong>WEB2091 App</strong>
+          </Link>
+
+          <div className="hidden md:flex items-center space-x-8">
+            <Link to="#" className="hover:text-gray-200">
+              Trang chủ
+            </Link>
+            <Link to="/list" className="hover:text-gray-200">
+              Danh sách
+            </Link>
+            <Link to="/add" className="hover:text-gray-200">
+              Thêm mới
+            </Link>
+          </div>
+
+          <div className="hidden md:flex items-center space-x-6">
+            <Link to="#" className="hover:text-gray-200">
+              Đăng nhập
+            </Link>
+            <Link to="#" className="hover:text-gray-200">
+              Đăng ký
+            </Link>
+          </div>
+        </div>
+      </nav>
+
+      {/* MAIN CONTENT */}
+      <div className="max-w-6xl mx-auto mt-10 px-4 text-center">
+        <h1 className="text-4xl font-bold mb-4">Chào mừng đến với WEB2091</h1>
+      </div>
+      <div>
+      <Layout>
+      <Header style={{ color: "white" }}>Header</Header>
+      <Content style={{ padding: 20 }}>
+        <h1>form </h1>
+            <Form layout="vertical" style={{ maxWidth: 400 }} onFinish={onFinish}>
+                            <Form.Item label="title" name='title'
+                                rules={[
+                                    { required: true, message: "vui lòng nhập title" },
+                                ]}>
+                                <Input />
+                            </Form.Item>
+                            <Form.Item label="description" name='description'
+                                rules={[
+                                    { required: true, message: "vui lòng nhập Description " },
+                                ]}>
+                                <Input />
+                            </Form.Item>
+                            <Form.Item label="author" name='author'
+                                rules={[
+                                    { required: true, message: "vui lòng nhập Description " },
+                                ]}>
+                                <Input />
+                            </Form.Item>
+                            <Form.Item 
+                                label="Danh mục" 
+                                name="categoryId"
+                                rules={[{ required: true, message: "Vui lòng chọn danh mục" }]}>
+                                <Select
+                                    loading={isLoading}
+                                    placeholder="Chọn danh mục"
+                                    options={categories?.map((item: Category) => ({
+                                        label: item.title,
+                                        value: item.id
+                                    }))}
+                                />
+                            </Form.Item>
+                            <Form.Item label="Active" name='Active' valuePropName="checked">
+                                <Checkbox/>
+                            </Form.Item>
+                            <Form.Item>
+                                <Button type='primary' htmlType='submit' loading={isPending} >
+                                     {isPending ? "Đang gửi" : "Đăng"}
+                                </Button>
+                            </Form.Item>
+                        </Form>
+      </Content>
+      <Footer>Footer</Footer>
+    </Layout>
+    </div>
+      <Toaster />
+
+            </>
+    )
+}
